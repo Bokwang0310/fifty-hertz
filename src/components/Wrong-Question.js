f7eed2ce2c0c7bfc9f1453f9722036dfc81397e2
Bokwang0310 <chdl2353@naver.com>
2022-11-27 05:26:19 +0900

    Add yt player
     유튜브 iframe 추가하고 센터 정렬

--- src/components/Wrong-Question.js
+++ src/components/Wrong-Question.js
@@ -5,15 +5,35 @@ import Paper from "@mui/material/Paper";
 import Image from "../imgs/wrong-question.jpg"; // Import using relative path
 
 const styles = {
-  paperContainer: {
+  backgroundImg: {
+    display: "flex",
     backgroundImage: `url(${Image})`,
     height: "100vh",
     backgroundSize: "cover",
+    alignItems: "center",
+    justifyContent: "center",
+    // filter: `brightness(30%)`,
   },
+  yt: {},
 };
 
 function WrongQuestion() {
-  return <Paper style={styles.paperContainer} />;
+  return (
+    <>
+      {/* <Paper /> */}
+      <Paper style={styles.backgroundImg}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/bi3lPMmK7Vk"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </Paper>
+    </>
+  );
 }
 
 export default WrongQuestion;
